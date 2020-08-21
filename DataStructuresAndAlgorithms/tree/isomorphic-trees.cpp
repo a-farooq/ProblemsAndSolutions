52629630759c3b8a7ee8c90c2fc938f29eff280d
Aamil Farooq <aamil.farooq@gmail.com>
2020-08-21 18:32:57 +0530

    added another solution

--- DataStructuresAndAlgorithms/tree/isomorphic-trees.cpp
+++ DataStructuresAndAlgorithms/tree/isomorphic-trees.cpp
@@ -49,8 +49,30 @@ struct Node {
 };
 */
 
+bool isIsomorphic(Node* n1, Node *n2)
+{
+ // Both roots are NULL, trees isomorphic by definition
+ if (n1 == NULL && n2 == NULL)
+    return true;
+
+ // Exactly one of the n1 and n2 is NULL, trees not isomorphic
+ if (n1 == NULL || n2 == NULL)
+    return false;
+
+ if (n1->data != n2->data)
+    return false;
+
+ // There are two possible cases for n1 and n2 to be isomorphic
+ // Case 1: The subtrees rooted at these nodes have NOT been "Flipped".
+ // Both of these subtrees have to be isomorphic, hence the &&
+ // Case 2: The subtrees rooted at these nodes have been "Flipped"
+ return
+ (isIsomorphic(n1->left,n2->left) && isIsomorphic(n1->right,n2->right))||
+ (isIsomorphic(n1->left,n2->right) && isIsomorphic(n1->right,n2->left));
+}
+
 // Return True if the given trees are isomotphic. Else return False.
-bool isIsomorphic(Node *root1,Node *root2)
+bool isIsomorphic2(Node *root1,Node *root2)
 {
     if(!root1 && !root2) return true;
     if((root1 && !root2) || (!root1 && root2)) return false;
