df13f722dc4c16727fefd810ca43d43d81d46550
Aamil Farooq <aamil.farooq@gmail.com>
2020-09-10 23:15:52 +0530

    sort stack using another stack

---
+++ DataStructuresAndAlgorithms/stack/sort-with-aux-stack.cpp
@@ -0,0 +1,52 @@
+/****
+Given a stack, the task is to sort it such that the top of the stack has the greatest element.
+
+Example 1:
+
+Input:
+Stack: 3 2 1
+Output: 3 2 1
+Example 2:
+
+Input:
+Stack: 11 2 32 3 41
+Output: 41 32 11 3 2
+Your Task: 
+You don't have to read input or print anything. Your task is to complete the function sort() which sorts the elements present in the given stack. (The sorted stack is printed by the driver's code by popping the elements of the stack.)
+
+Expected Time Complexity : O(N*N)
+Expected Auixilliary Space : O(N) recursive.
+
+Constraints:
+1<=N<=100
+
+  ****/
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+void SortedStack :: sort()
+{
+   //Your code here
+	stack<int> s2;
+
+	while(!s.empty())
+	{
+		int t1 = s.top();
+		s.pop();
+
+		while(!s2.empty() && s2.top() > t1)
+		{
+			s.push(s2.top());
+			s2.pop();
+		}
+		s2.push(t1);
+	}
+}
