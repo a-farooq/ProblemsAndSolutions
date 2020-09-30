f992eadcda329cffb5a4e0338913e2fe6cf8f44b
Aamil Farooq <aamil.farooq@gmail.com>
2020-09-30 20:01:45 +0530

    added problem statement

--- DataStructuresAndAlgorithms/string/longest-common-subsequence.cpp
+++ DataStructuresAndAlgorithms/string/longest-common-subsequence.cpp
@@ -1,4 +1,36 @@
 /****
+Given two sequences, find the length of longest subsequence present in both of them. Both the strings are of uppercase.
+
+Input:
+First line of the input contains no of test cases  T,the T test cases follow.
+Each test case consist of 2 space separated integers A and B denoting the size of string str1 and str2 respectively
+The next two lines contains the 2 string str1 and str2 .
+
+Output:
+For each test case print the length of longest  common subsequence of the two strings .
+
+Constraints:
+1<=T<=200
+1<=size(str1),size(str2)<=100
+
+Example:
+Input:
+2
+6 6
+ABCDGH
+AEDFHR
+3 2
+ABC
+AC
+
+Output:
+3
+2
+
+Explanation
+LCS for input Sequences “ABCDGH” and “AEDFHR” is “ADH” of length 3.
+
+LCS of "ABC" and "AC" is "AC" of length 2
 
   ****/
 
