8ea065749c756e175d0c91bd36e1cf181ccc82e4
Aamil Farooq <aamil.farooq@gmail.com>
2020-10-02 15:06:04 +0530

    smallest positive missing number

---
+++ DataStructuresAndAlgorithms/arrays/smallest-positive-missing-num.cpp
@@ -0,0 +1,68 @@
+/****
+You are given an array arr[] of N integers including 0. The task is to find the smallest positive number missing from the array.
+Note: Expected solution in O(n) time using constant extra space.
+
+Input:
+First line consists of T test cases. First line of every test case consists of N, denoting the number of elements in array. Second line of every test case consists of elements in array.
+
+Output:
+Single line output, print the smallest positive number missing.
+
+Constraints:
+1 <= T <= 100
+1 <= N <= 10^6
+-10^6 <= arr[i] <= 10^6
+
+Example:
+Input:
+2
+5
+1 2 3 4 5
+5
+0 -10 1 3 -20
+Output:
+6
+2
+
+Explanation:
+Testcase 1: Smallest positive missing number is 6.
+Testcase 2: Smallest positive missing number is 2.
+
+Algorithm:
+insert all positive numbers into a set
+find each number starting from 1 till the max+1 inserted
+if a number is not found, this is the answer
+  ****/
+
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n;
+	    int maxi = 0;
+	    unordered_set<int> uset;
+	    cin >> n;
+	    for(int i=0; i<n; i++) {
+	        int m;
+	        cin >> m;
+	        if(m>0)
+	            uset.insert(m);
+	        maxi = max(maxi, m);
+	    }
+
+	    int i=1;
+	    for(; i<=maxi+1; i++)
+	    {
+	        if(uset.find(i) == uset.end()) {
+	            break;
+	        }
+	    }
+	    cout << i << endl;
+	}
+	return 0;
+}
