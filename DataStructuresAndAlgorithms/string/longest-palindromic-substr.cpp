642d2022ee2e03a40a8446353fb7b4bd06d4378c
Aamil Farooq <aamil.farooq@gmail.com>
2020-10-02 19:53:03 +0530

    added another approach

--- DataStructuresAndAlgorithms/string/longest-palindromic-substr.cpp
+++ DataStructuresAndAlgorithms/string/longest-palindromic-substr.cpp
@@ -65,6 +65,60 @@ string max_len_pal(string st)
     return st.substr(start,maxi);
 }
 
+// User function template for C++
+
+// A : given string
+// return the required string
+string longestPalindrome(string st)
+{
+    // code here
+    int stlen = st.length();
+    int maxi=0;
+    int maxi_start=0;
+    for(int k=0; k<stlen; k++) {
+        int i=k-1, j=k+1;
+        int len=1;
+        int start = k;
+        //cout << k << "=>"<< i << "=>" << j << endl;
+        //check for odd length palindromes
+        while(i>=0 && j<stlen) {
+            //cout << "====" << endl;
+            if(st[i]!=st[j]) break;
+            len+=2;
+            start=i;
+            i--;
+            j++;
+        }
+        if(maxi<len) {
+            maxi = len;
+            maxi_start = start;
+        }
+        //cout << len<< ", "<<maxi_start << ", "<< maxi << endl;
+        //check for even length palindromes
+        if(k<stlen-1 && st[k]==st[k+1]) {
+            i=k-1;
+            j=k+2;
+            len=2;
+            start=k;
+            while(i>=0 && j<stlen) {
+                if(st[i]!=st[j]) break;
+                len+=2;
+                start=i;
+                i--;
+                j++;
+            }
+            if(maxi<len) {
+                maxi = len;
+                maxi_start = start;
+            }
+            //cout << len<<", "<<maxi_start << ", "<< maxi << endl;
+        }
+
+    }
+    //cout << maxi_start << ", "<< maxi << endl;
+    return st.substr(maxi_start, maxi);
+}
+
 int main() {
 	int t;
 	cin >> t;
