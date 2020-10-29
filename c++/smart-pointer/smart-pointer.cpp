ded68f0a3009c320c479e5c709c5e722699ba042
Aamil Farooq <aamil.farooq@gmail.com>
2020-10-29 11:26:59 +0530

    added a folder

---
+++ c++/smart-pointer/smart-pointer.cpp
@@ -0,0 +1,53 @@
+
+
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+	string getName()
+	{
+		return "abc";
+	}
+};
+
+template<typename T>
+class SmartPtr
+{
+	T* m_ptr;
+public:
+	SmartPtr(T* a_ptr=nullptr) : m_ptr(a_ptr)
+	{
+	}
+
+	T* operator->()
+	{
+		return m_ptr;
+	}
+
+	T& operator*()
+	{
+		return *m_ptr;
+	}
+
+	~SmartPtr()
+	{
+		delete m_ptr;
+	}
+
+};
+
+void fun(SmartPtr<Student>& sp)
+{
+	cout << sp->getName() << endl;
+}
+
+int main()
+{
+	SmartPtr<Student> sp(new Student());
+	cout << sp->getName() << endl;
+	cout << (*sp).getName() << endl;
+	
+	fun(sp);
+}
